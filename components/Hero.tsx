3fe2896994a47f08e53ab47a5297217e2466608d
iamtonmoy0 <tonmoy2code@gmail.com>
2024-01-12 02:46:18 +0600

    footer added

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,23 +1,27 @@
 "use client";
 import React from "react";
 import CustomButton from "./CustomButton";
+import Image from "next/image";
 
 export default function () {
   const handleScroll = () => {};
+
   return (
-    <div className=" w-full  flex justify-center">
-      <div>
-        <h1 className=" text-4xl text-bold ">
-          Find ,Book , Rent A car- fast and quick
+    <div className="w-full flex flex-col items-center pt-16">
+      <div className="self-center ">
+        <h1 className="text-4xl font-bold">
+          Find, Book, Rent A car - fast and quick
         </h1>
         <p className="text-md">Streamline car for your best user experience!</p>
         <CustomButton
           title="Explore Cars"
-          containerStyles="bg-blue-500 text-white p-3  mt-5 rounded-full "
+          containerStyles="bg-blue-500 text-white p-3 mt-5 rounded-full"
           handleClick={handleScroll}
         />
       </div>
-      <div></div>
+      <div className="mx-auto mt-5">
+        <Image src="/hero.png" alt="hero" width={400} height={400} />
+      </div>
     </div>
   );
 }
--- types/index.ts
+++ types/index.ts
@@ -3,5 +3,6 @@ import { MouseEventHandler } from "react";
 export interface CustomButtonProps {
   title: string;
   containerStyles?: string;
+  btnType?: "submit" | "button";
   handleClick?: MouseEventHandler<HTMLButtonElement>;
 }
